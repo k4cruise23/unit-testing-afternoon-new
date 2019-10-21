361cdc4ad3fb9f7ad6e628350e3413fdede235c2
Katie Cruise <katiecruise23@gmail.com>
2019-10-21 16:03:49 -0600

    completed afternoon testing

---
+++ src/__tests__/utilFunctions.test.js
@@ -0,0 +1,31 @@
+import {shortenText} from '../utils/functions'
+import {wordCount, attachUsername} from '../../server/utils'
+import {shortText, longText, posts, users} from './__data__/testData'
+
+
+it('shortenText should not alter a string with less than 100 characters', () => {
+    expect(shortenText(shortText)).toHaveLength(29)
+})
+
+it('shortenText shouls cut off extra characters after 100 and add three periods', () => {
+    const shortened = shortenText(longText)
+    expect(shortened).not.toHaveLength(longText.length)
+    expect(shortened.slice(-3)).toBe('...')
+})
+
+it('wordCount should correctly count the number of words in a sentence', () => {
+    expect(wordCount(posts)).toBe(233)
+})
+
+it('attachUserName should correctly attach a users full name to a post', () => {
+    const newPosts = attachUsername(users, posts)
+    expect(newPosts[0]).toHaveProperty('displayName')
+})
+
+it('attachUserName should remove any post with no matching user', () => {
+    const newPosts = attachUsername(users, posts)
+    const deletedPost = posts[5]
+    expect(newPosts).not.toContainEqual(deletedPost)
+})
+
+
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,18 @@
-export const shortenText = text => {
-    let shortened = text.substr(0, 100).trim();
-    shortened.length === 100 && (shortened += '...');
-    return shortened;
-  };
+module.exports = {
+  wordCount(posts) {
+    return posts.reduce((a, v) => (a += v.text.split(' ').length), 0);
+  },
+  attachUserName(users, posts) {
+    let userDict = users.reduce((a, v) => {
+      a[v.id] = v;
+      return a;
+    }, {});
+    return posts
+      .filter(p => userDict[p.userId])
+      .map(p => {
+        p.displayName = `${userDict[p.userId].first} ${userDict[p.userId].last}`;
+        return p;
+      });
+  },
+};
   
